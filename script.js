de0b9105f89561cf25e6914b0ece50fb6692302b
Dayvson <dayvsonnlacerda360@gmail.com>
2024-11-20 10:43:25 -0300

    Troca de cenários 2
    Agora, tudo está ajustado no site, só preciso pegar alguns conteúdos para colocar conforme o andar atual.

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function tocontact() {
     foot2.style.display = 'none';
     foot3.style.display = 'none';
     foot4.style.display = 'block';
-    
+
     home.style.color = 'black';
     projects.style.color = 'black';
     more.style.color = 'black';
@@ -73,75 +73,88 @@ let ir = document.getElementById('avancar');
 
 let texto = document.getElementById('moltext');
 let atras = document.getElementById('moldown');
+let papelada = document.getElementById('projetos1');
+let linkado = document.getElementById('linkado');
 
 proximo = () => {
-    andar++ ; 
+    andar++;
 
     if (andar == 1) {
-        cenario2(); 
+        cenario2();
     }
 
     if (andar == 2) {
-        cenario3(); 
+        cenario3();
     }
 
     if (andar >= 3) {
-        andar = 0; 
+        andar = 0;
         cenario1();
     }
 };
 
 
-voltar = () => { 
+voltar = () => {
     andar--;
 
     if (andar == 0) {
-        cenario1(); 
+        cenario1();
     }
 
     if (andar == 1) {
-        cenario2(); 
+        cenario2();
     }
 
     if (andar <= -1) {
-        andar = 2; 
+        andar = 2;
         cenario3();
     }
 };
 
-cenario1 = () => { 
-                   texto.textContent = 'BitBlog' ; 
-                   texto.style.left = '100px';
-                   atras.src = 'bitblog_home.png'; 
-                 };
+cenario1 = () => {
+    texto.textContent = 'BitBlog';
+    texto.style.left = '100px';
+    atras.src = 'bitblog_home.png';
 
-cenario2 = () => { 
-                  texto.textContent = 'Psicoajuda' ; 
-                  texto.style.left = '75px';
-                  atras.src = 'psicoajuda_home.png';
-               
-                 };
+    papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> O BitBlog tem esse nome, justamente por ser um site da categoria de "blog", em que conteúdos são postados periodicamente. Os conteúdos que eu publico nele são de temas diversos, sempre coisas que eu acho interessante, como psicologia, literatura e (é claro) jogos. Ele tem o lado `blog` e o lado `study`, trazendo o tema, como a inspiração por uma escritora famosa, e umas informações sobre, mas ambos vêm junto de uma reflexão ';
+    
+    linkado.href = 'https://bitblog-dolv.onrender.com/';
+};
 
-cenario3 = () => { 
-                  texto.textContent = 'MoonHead' ; 
-                  texto.style.left = '75px';
-                  atras.src = 'moonhead_home.png';
-                };
+cenario2 = () => {
+    texto.textContent = 'Psicoajuda';
+    texto.style.left = '75px';
+    atras.src = 'psicoajuda_home.png';
+    
+    papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> PsicoAjuda foi o projeto do trabalho de conclusão de curso que eu fiz do meu curso técnico no ano de 2023. Esse projeto teve muitas partes envolvidas e eu fiquei responsável pela parte Web. Com ele, tentamos combater a desinformação acerca de patologias mentais, mostrando que é algo que precisa de olhares mais atentos nestes momentos em que vivemos.';
+    
+    linkado.href = 'https://psicoajuda.onrender.com';
+};
+
+cenario3 = () => {
+    texto.textContent = 'BirdsRun';
+    texto.style.left = '75px';
+    atras.src = 'moonhead_home.png';
+
+    papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> Foi o meu primeiro jogo que eu criei. Desenvolvi no programa GameMaker e ele é feito para pc. Conforme você pega os itens que estão espalhados pelo mapa, você consegue passar para a próxima fase, mas pra isso é preciso se esquivar das galinhas furiosas!!!';
+
+    linkado.href = 'https://portalact5.wordpress.com/';
+};
 
 
-ir.addEventListener('click',proximo);
-vir.addEventListener('click',voltar);
+ir.addEventListener('click', proximo);
+vir.addEventListener('click', voltar);
 
 
 
-home.addEventListener('click',tohome);
-projects.addEventListener('click',toprojects);
-more.addEventListener('click',tomore);
-contact.addEventListener('click',tocontact);
+home.addEventListener('click', tohome);
+projects.addEventListener('click', toprojects);
+more.addEventListener('click', tomore);
+contact.addEventListener('click', tocontact);
 
 
 
-home.addEventListener('click',tohome);
-projects.addEventListener('click',toprojects);
-more.addEventListener('click',tomore);
-contact.addEventListener('click',tocontact);
+home.addEventListener('click', tohome);
+projects.addEventListener('click', toprojects);
+more.addEventListener('click', tomore);
+contact.addEventListener('click', tocontact);

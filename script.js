b17ffd8ea298ecd2d73c8e1112c70eb0e5f77c38
Dayvson <dayvsonnlacerda360@gmail.com>
2024-11-23 11:15:07 -0300

    Concluído!!!
    Toda a estrutura e coisas que haviam no meu protótipo já está implementado neste site, com funcionalidades e páginas interativas. Conforme o tempo se passa eu irei adicionar outros projetos e etc, mas o iglu já está completo!

--- script.js
+++ script.js
@@ -76,6 +76,10 @@ let atras = document.getElementById('moldown');
 let papelada = document.getElementById('projetos1');
 let linkado = document.getElementById('linkado');
 
+let gif1 = document.getElementById('ogif1');
+let gif2 = document.getElementById('ogif2');
+let gif3 = document.getElementById('ogif3');
+
 proximo = () => {
     andar++;
 
@@ -111,34 +115,48 @@ voltar = () => {
     }
 };
 
-cenario1 = () => {
+cenario3 = () => {
     texto.textContent = 'BitBlog';
     texto.style.left = '100px';
     atras.src = 'bitblog_home.png';
 
     papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> O BitBlog tem esse nome, justamente por ser um site da categoria de "blog", em que conteúdos são postados periodicamente. Os conteúdos que eu publico nele são de temas diversos, sempre coisas que eu acho interessante, como psicologia, literatura e (é claro) jogos. Ele tem o lado `blog` e o lado `study`, trazendo o tema, como a inspiração por uma escritora famosa, e umas informações sobre, mas ambos vêm junto de uma reflexão ';
-    
+    papelada.style.margin = '0px';
+
     linkado.href = 'https://bitblog-dolv.onrender.com/';
+    gif1.src = 'bb2.gif';
+    gif2.src = 'bb1.gif';
+    gif3.src = 'bb3.gif';
 };
 
-cenario2 = () => {
+window.onload = cenario1 = () => {
     texto.textContent = 'Psicoajuda';
     texto.style.left = '75px';
     atras.src = 'psicoajuda_home.png';
     
     papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> PsicoAjuda foi o projeto do trabalho de conclusão de curso que eu fiz do meu curso técnico no ano de 2023. Esse projeto teve muitas partes envolvidas e eu fiquei responsável pela parte Web. Com ele, tentamos combater a desinformação acerca de patologias mentais, mostrando que é algo que precisa de olhares mais atentos nestes momentos em que vivemos.';
-    
+    papelada.style.margin = '20px 0 20px 0';
+
     linkado.href = 'https://psicoajuda.onrender.com';
+    gif1.src = 'pv1.gif';
+    gif2.src = 'pv2.gif';
+    gif3.src = 'pv3.png';
 };
 
-cenario3 = () => {
+cenario2 = () => {
     texto.textContent = 'BirdsRun';
     texto.style.left = '75px';
     atras.src = 'moonhead_home.png';
 
     papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> Foi o meu primeiro jogo que eu criei. Desenvolvi no programa GameMaker e ele é feito para pc. Conforme você pega os itens que estão espalhados pelo mapa, você consegue passar para a próxima fase, mas pra isso é preciso se esquivar das galinhas furiosas!!!';
+    papelada.style.marginTop = '40px 0 40px 0';
 
     linkado.href = 'https://portalact5.wordpress.com/';
+
+    gif1.src = 'mv1.gif';
+    gif2.src = 'mv2.gif';
+    gif3.src = 'mv3.gif';
+
 };
 
 
@@ -158,3 +176,4 @@ home.addEventListener('click', tohome);
 projects.addEventListener('click', toprojects);
 more.addEventListener('click', tomore);
 contact.addEventListener('click', tocontact);
+

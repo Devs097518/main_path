3611a7bfd3594bd78cae415eb680c4bd461bb1d5
Dayvson <dayvsonnlacerda360@gmail.com>
2024-12-12 12:23:50 -0300

    Pequenos ajustes
    Eu apaguei várias linhas em branco e comentários grandes que ocupavam muito espaço e isso atrapalhava na performace do site, no sentido de que quando a gente abria, demorava muito para carregar e eu acredito que isso ainda não foi o suficiente. Também coloquei um ícone no repositório que é para aparecer sempre que eu compartilhar a website. Então, em vez de aparecer aquela nuvem que está no topo do site, vai aparecer o ícone que eu fiz.

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ var projects = document.getElementById('projects');
 var contact = document.getElementById('contact');
 var more = document.getElementById('more');
 
-
 var foot1 = document.getElementById('foot1');
 var foot2 = document.getElementById('foot2');
 var foot3 = document.getElementById('foot3');
 var foot4 = document.getElementById('foot4');
-
 
 
 function tohome() {
@@ -28,7 +26,6 @@ function toprojects() {
     foot2.style.display = 'flex';
     foot3.style.display = 'none';
     foot4.style.display = 'none';
-
 
     home.style.color = 'black';
     projects.style.color = 'purple';
@@ -50,8 +47,6 @@ function tomore() {
 }
 
 
-
-
 function tocontact() {
     foot1.style.display = 'none';
     foot2.style.display = 'none';
@@ -63,8 +58,6 @@ function tocontact() {
     more.style.color = 'black';
     contact.style.color = 'purple'
 }
-
-
 
 
 var andar = 0;
@@ -119,10 +112,8 @@ cenario3 = () => {
     texto.textContent = 'BitBlog';
     texto.style.left = '100px';
     atras.src = 'bitblog_home.png';
-
     papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> O BitBlog tem esse nome, justamente por ser um site da categoria de "blog", em que conteúdos são postados periodicamente. Os conteúdos que eu publico nele são de temas diversos, sempre coisas que eu acho interessante, como psicologia, literatura e (é claro) jogos. Ele tem o lado `blog` e o lado `study`, trazendo o tema, como a inspiração por uma escritora famosa, e umas informações sobre, mas ambos vêm junto de uma reflexão ';
     papelada.style.margin = '0px';
-
     linkado.href = 'https://bitblog-dolv.onrender.com/';
     gif1.src = 'bb2.gif';
     gif2.src = 'bb1.gif';
@@ -133,10 +124,8 @@ window.onload = cenario1 = () => {
     texto.textContent = 'Psicoajuda';
     texto.style.left = '75px';
     atras.src = 'psicoajuda_home.png';
-    
     papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> PsicoAjuda foi o projeto do trabalho de conclusão de curso que eu fiz do meu curso técnico no ano de 2023. Esse projeto teve muitas partes envolvidas e eu fiquei responsável pela parte Web. Com ele, tentamos combater a desinformação acerca de patologias mentais, mostrando que é algo que precisa de olhares mais atentos nestes momentos em que vivemos.';
     papelada.style.margin = '20px 0 20px 0';
-
     linkado.href = 'https://psicoajuda.onrender.com';
     gif1.src = 'pv1.gif';
     gif2.src = 'pv2.gif';
@@ -147,16 +136,12 @@ cenario2 = () => {
     texto.textContent = 'BirdsRun';
     texto.style.left = '75px';
     atras.src = 'moonhead_home.png';
-
     papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> Foi o meu primeiro jogo que eu criei. Desenvolvi no programa GameMaker e ele é feito para pc. Conforme você pega os itens que estão espalhados pelo mapa, você consegue passar para a próxima fase, mas pra isso é preciso se esquivar das galinhas furiosas!!!';
     papelada.style.marginTop = '40px 0 40px 0';
-
     linkado.href = 'https://portalact5.wordpress.com/';
-
     gif1.src = 'mv1.gif';
     gif2.src = 'mv2.gif';
     gif3.src = 'mv3.gif';
-
 };
 
 
@@ -164,12 +149,10 @@ ir.addEventListener('click', proximo);
 vir.addEventListener('click', voltar);
 
 
-
 home.addEventListener('click', tohome);
 projects.addEventListener('click', toprojects);
 more.addEventListener('click', tomore);
 contact.addEventListener('click', tocontact);
-
 
 
 home.addEventListener('click', tohome);

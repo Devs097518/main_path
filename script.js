f38a22e91b6976be0859c5d95666c9a1e5c5419d
Dayvson <dayvsonnlacerda360@gmail.com>
2024-12-23 11:03:02 -0300

    Novos projetos
    Adicionei outros projetos que eu venho criado.

--- script.js
+++ script.js
@@ -84,7 +84,19 @@ proximo = () => {
         cenario3();
     }
 
-    if (andar >= 3) {
+    if (andar == 3) {
+        cenario4();
+    }
+
+    if (andar == 4) {
+        cenario5();
+    }
+
+    if (andar == 5) {
+        cenario6();
+    }
+
+    if (andar >= 6) {
         andar = 0;
         cenario1();
     }
@@ -102,22 +114,35 @@ voltar = () => {
         cenario2();
     }
 
-    if (andar <= -1) {
-        andar = 2;
+    if (andar == 2) {
         cenario3();
+    }
+
+    if (andar == 3) {
+        cenario4();
+    }
+
+    if (andar == 4) {
+        cenario5();
+    }
+
+    if (andar <= -1) {
+        andar = 5;
+        cenario6();
     }
 };
 
 cenario3 = () => {
-    texto.textContent = 'BitBlog';
-    texto.style.left = '100px';
-    atras.src = 'bitblog_home.png';
-    papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> O BitBlog tem esse nome, justamente por ser um site da categoria de "blog", em que conteúdos são postados periodicamente. Os conteúdos que eu publico nele são de temas diversos, sempre coisas que eu acho interessante, como psicologia, literatura e (é claro) jogos. Ele tem o lado `blog` e o lado `study`, trazendo o tema, como a inspiração por uma escritora famosa, e umas informações sobre, mas ambos vêm junto de uma reflexão ';
+   
+    texto.textContent = 'Coin Conversor';
+    texto.style.left = '80px';
+    atras.src = 'cccoin.png';
+    papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> O Coin Conversor é uma calculadora de moedas internacionais, onde o valor do câmbio é atualizado diariamente com a ajuda de servidores externos, então não há preocupação pois cada conversão entre os valores estarão em dia. Isso graças à integração de API, que traz dados atualizados de servidores externos para fazer as operações.';
     papelada.style.margin = '0px';
-    linkado.href = 'https://bitblog-dolv.onrender.com/';
-    gif1.src = 'bb2.gif';
-    gif2.src = 'bb1.gif';
-    gif3.src = 'bb3.gif';
+    linkado.href = 'https://coin-conversor.onrender.com/';
+    gif1.src = 'cclogo.png';
+    gif2.src = 'ccg2.png';
+    gif3.src = 'ccg3.png';
 };
 
 window.onload = cenario1 = () => {
@@ -143,6 +168,47 @@ cenario2 = () => {
     gif2.src = 'mv2.gif';
     gif3.src = 'mv3.gif';
 };
+
+cenario4 = () => {
+    texto.textContent = 'FreeShop';
+    texto.style.left = '80px';
+    atras.src = 'fsg1.png';
+    papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> O FreeShop é dedicado à promoção de doações para pessoas que precisam. É comum que você tenha algo útil mas que está empoeirando no fundo da garagem, sabia que existe alguém que precisa muito daquilo? Faça um gesto de caridade e o doe através deste site e deixe o dia de uma pessoa melhor. ';
+    papelada.style.margin = '0px';
+    linkado.href = 'https://freeshop.onrender.com/';
+    gif1.src = 'Freeshop.png';
+    gif2.src = 'fsg2.png';
+    gif3.src = 'fsg3.png';
+};
+
+cenario5 = () => {
+    texto.textContent = 'Diary_Note';
+    texto.style.left = '75px';
+    atras.src = 'dng1.png';
+    papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> Escrever e registrar como foi o seu dia é importante para umas mil coisas, ajuda a perceber melhor os problemas à sua volta e a entender a maneira como você pensava em determinado momento. Aqui você pratica a escrita online, que possui muitas vantagens, como ter registros mais privacidade e capaz de guardar em pastas específicas, tanto localmente quanto na núvem, sendo capaz de permanecer com aquelas informações que você escreveu de forma mais organizada e segura. ';
+    papelada.style.margin = '0px';
+    linkado.href = 'https://diary-note.onrender.com/';
+    gif1.src = 'dng3.png';
+    gif2.src = 'dng1.png';
+    gif3.src = 'dng2.png';
+};
+
+cenario6 = () => {
+    texto.textContent = 'BitBlog';
+    texto.style.left = '100px';
+    atras.src = 'bitblog_home.png';
+    papelada.innerHTML = '<img src="star-removebg-preview.png" alt="" id="star_img"> O BitBlog tem esse nome, justamente por ser um site da categoria de "blog", em que conteúdos são postados periodicamente. Os conteúdos que eu publico nele são de temas diversos, sempre coisas que eu acho interessante, como psicologia, literatura e (é claro) jogos. Ele tem o lado `blog` e o lado `study`, trazendo o tema, como a inspiração por uma escritora famosa, e umas informações sobre, mas ambos vêm junto de uma reflexão ';
+    papelada.style.margin = '0px';
+    linkado.href = 'https://bitblog-dolv.onrender.com/';
+    gif1.src = 'bb2.gif';
+    gif2.src = 'bb1.gif';
+    gif3.src = 'bb3.gif';
+};
+
+
+
+
+// https://diary-note.onrender.com/
 
 
 ir.addEventListener('click', proximo);
